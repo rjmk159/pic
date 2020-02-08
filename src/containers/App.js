b4733b68bb37b692698883a0a295ba041d60a66f
Raja Junaid <rajajunaid@Rajas-MacBook-Pro.local>
2020-02-08 13:42:11 +0530

    removed bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import MainApp from '../containers/MainApp';
-import AppContainer from '../assets/css/App.style'
 
 class App extends React.Component{
   render(){
     return(
-      <AppContainer>
+      <div>
         <Header />
         <MainApp />
-      </AppContainer>
+      </div>
     )
   }
 }
